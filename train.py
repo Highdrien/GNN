557339eca0bcc02245fdaa9b302693380f2977c5
Highdrien <adrien.zabban@outlook.fr>
2023-10-20 20:46:34 +0200

    save best only

--- train.py
+++ train.py
@@ -1,32 +1,36 @@
+import os
+import numpy as np
+from icecream import ic
+from typing import Optional
+
 import torch
 import torch.nn.functional as F
 from torchmetrics.classification import Accuracy
 
-from icecream import ic
-import numpy as np
-from typing import List, Dict, Optional
-
 from dataloader import DataGraph
 from model import getModel
+from utils import save_experiement, save_logs
 
 
-MODELS = {'GCN': {'hidden_layer': [64, 32], 'dropout': 0.1},
-          'SGC': {'hidden_layer': [64, 32], 'dropout': 0.1},
-          'GIN': {'epsilon': 1e-15, 'hidden_layer': 64}}
+MODELS = {'GCN': {'hidden_layer': [16, 32], 'dropout': 0.1},
+          'SGC': {'hidden_layer': [128, 64], 'dropout': 0.1},
+          'GIN': {'epsilon': 1e-5, 'hidden_layer': 64}}
 
 
 def train(model_name: str,
           num_epochs: int,
           learning_rate: float,
           dataset_name: Optional[str]='ENZYMES'
-          ) -> Dict[str, Dict[str, List[float]]]:
+          ) -> None:
+    
+    assert model_name in MODELS.keys(), f"Error, model_name must be in {list(MODELS.keys())} but is {model_name}"
     
     train_generator = DataGraph(mode='train', dataset_name=dataset_name)
     val_generator = DataGraph(mode='val', dataset_name=dataset_name)
 
     # Get Model
-    model = getModel(MODELS=MODELS,
-                     model_name=model_name,
+    model = getModel(model_name=model_name,
+                     model_info=MODELS[model_name],
                      num_features=train_generator.num_features,
                      num_classes=train_generator.num_classes)
     print(model)
@@ -34,6 +38,12 @@ def train(model_name: str,
     criterion = torch.nn.CrossEntropyLoss(reduction='mean')
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     acc_metric = Accuracy(task='multiclass', num_classes=train_generator.num_classes)
+
+    logging_path = save_experiement(model_name=model_name,
+                                    dataset_name=dataset_name,
+                                    learning_rate=learning_rate,
+                                    model_info=MODELS[model_name])
+    ic(logging_path)
 
     learning = {'train': {'loss': [], 'acc': []},
                 'val': {'loss': [], 'acc': []}}
@@ -64,7 +74,7 @@ def train(model_name: str,
         val_acc = []
         with torch.no_grad():
             for i in range(len(val_generator)):
-                x, y_true = train_generator.__getitem__(i)
+                x, y_true = val_generator.__getitem__(i)
                 y_pred = model.forward(x)
 
                 loss = criterion(y_pred.unsqueeze(0), y_true.unsqueeze(0))
@@ -76,14 +86,19 @@ def train(model_name: str,
         learning['val']['loss'].append(np.mean(val_loss))
         learning['val']['acc'].append(np.mean(val_acc))
 
+        # save weigth only if it's the best validation loss
+        if learning['val']['loss'][-1] == min(learning['val']['loss']):
+            print("save weigth")
+            torch.save(model.state_dict(), os.path.join(logging_path, 'checkpoint.pt'))
+
         print(f"VAL   -> loss: {learning['val']['loss'][-1]: .2f} \tacc: {learning['val']['acc'][-1]: .2f}")
     
-    return learning
+    save_logs(logpath=logging_path, learning=learning)
 
 
 
 if __name__ == '__main__':
     # Comment lines
-    train(model_name='GCN', num_epochs=5, learning_rate=0.01)
-    train(model_name='SGC', num_epochs=5, learning_rate=0.01)
-    train(model_name='GIN', num_epochs=5, learning_rate=0.01)
+    train(model_name='GCN', num_epochs=10, learning_rate=0.01)
+    # train(model_name='SGC', num_epochs=10, learning_rate=0.01)
+    train(model_name='GIN', num_epochs=10, learning_rate=0.01)
